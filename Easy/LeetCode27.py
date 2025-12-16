9ad04dc094a40efa7eadc6dc555f4c3f200d4856
Awww-nanguasu <2328194470@qq.com>
2025-12-16 14:02:33 +0800

    '[Easy]: removeElement LeetCode27'

---
+++ Easy/LeetCode27.py
@@ -0,0 +1,15 @@
+
+#Double pointer ⭐⭐⭐
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        slow = fast = 0
+        while(fast<len(nums)):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        
+        if slow == 0:
+            nums = list()
+                
+        return slow
