1cea76d3ae4cdd40f40acaea62ceb48d1988f6ee
Awww-nanguasu <2328194470@qq.com>
2025-12-31 23:03:15 +0800

    'Daily problem 2'

--- Easy/LeetCode83.py
+++ Easy/LeetCode83.py
@@ -18,3 +18,23 @@ class Solution:
             pre.next = None
         return flag
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        pre = head # pre节点用于串联新链表
+        flag = pre
+        while head and head.next:
+            # 如果后一个节点 与 当前节点不同， 连上
+            if head.next.val != pre.val:  
+                pre.next = head.next
+                pre = pre.next
+            head = head.next
+        # 结尾
+        if pre:
+            pre.next = None
+        return flag
+
