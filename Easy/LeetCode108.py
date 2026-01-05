1df6720d1a5027265e4dff9d170199ce75ab5cec
Awww-nanguasu <2328194470@qq.com>
2026-01-05 22:43:14 +0800

    '[Easy] sortedArrayToBST LeetCode108'

--- Easy/LeetCode108.py
+++ Easy/LeetCode108.py
@@ -37,3 +37,19 @@ class Solution:
         return helper(0, len(nums) - 1)
 
 
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        def helper(left, right):
+            if left > right:
+                return None
+
+            # 选择任意一个中间位置数字作为根节点
+            mid = (left + right + randint(0, 1)) // 2
+
+            root = TreeNode(nums[mid])
+            root.left = helper(left, mid - 1)
+            root.right = helper(mid + 1, right)
+            return root
+
+        return helper(0, len(nums) - 1)
+
