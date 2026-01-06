fe699060d4c40a52a78bcee8bd2d168eff5053c4
Awww-nanguasu <2328194470@qq.com>
2026-01-06 23:03:33 +0800

    '[Easy] isBalanced LeetCode110'

--- Easy/LeetCode110.py
+++ Easy/LeetCode110.py
@@ -36,3 +36,21 @@ class Solution:
 
         return height(root) >= 0
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        return abs(self.height(root.right)-self.height(root.left))<2 and self.isBalanced(root.left) and self.isBalanced(root.right)
+	# 求高度
+    def height(self, node):
+        if not node:
+            return 0
+        return 1+max(self.height(node.right),self.height(node.left))
+
+
