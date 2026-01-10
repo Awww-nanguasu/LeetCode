9eb7cfeeefbd6e21870450677075aff14c15bd1e
Awww-nanguasu <2328194470@qq.com>
2026-01-10 11:42:38 +0800

    '[Moderate] reverse LeetCode07 '

--- Moderate/LeetCode07.py
+++ Moderate/LeetCode07.py
@@ -1,17 +1,36 @@
 
 # str2int 2 star
-def reverse(x: int) -> int:    
-    if abs(x) < 10:
-        return x
+# def reverse(x: int) -> int:    
+#     if abs(x) < 10:
+#         return x
 
-    str_x = str(x)
+#     str_x = str(x)
 
-    if str_x[0] == '-':
-        str_x = '-' + str_x[:0:-1]
-    else:
-        str_x = str_x[::-1]
+#     if str_x[0] == '-':
+#         str_x = '-' + str_x[:0:-1]
+#     else:
+#         str_x = str_x[::-1]
         
-    x = int(str_x)
-    if x < -2**31 or x > 2**31 - 1:
-        return 0
-    return x
+#     x = int(str_x)
+#     if x < -2**31 or x > 2**31 - 1:
+#         return 0
+#     return x
+
+# Failed 
+# In Python language: -123%10= -7 
+def reverse(x: int) -> int:    
+        res = 0
+        while x!=0:
+            tmp = x%10
+            if (res< -2**31 // 10) or (tmp<-8):
+                return 0
+            
+            if (res> (2**31 - 1) // 10) or (tmp>7):
+                return 0
+        
+            res = res*10 + tmp
+            print(res)
+            x //= 10
+        return res
+
+print(reverse(-123))
