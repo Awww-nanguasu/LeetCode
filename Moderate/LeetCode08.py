10c58bd97a8a7de8f585aa91d8bcfe46615375e3
Awww-nanguasu <2328194470@qq.com>
2026-01-11 15:15:11 +0800

    '[Moderate] myAtoi LeetCode 08 '

--- Moderate/LeetCode08.py
+++ Moderate/LeetCode08.py
@@ -34,4 +34,10 @@ class Solution:
             return res
         else:
             return -res
+
+# Regular Expression: 4 star. Here re is the Regular Expression module in Python
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        return max(min(int(*re.findall('^[\+\-]?\d+', s.lstrip())), 2**31 - 1), -2**31)
+
             
