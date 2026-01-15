6e617c5223cb0d0dfc66e1e2b48ef6e0e51887ae
Awww-nanguasu <2328194470@qq.com>
2026-01-15 20:31:03 +0800

    ‘Daily problem 1’

--- Moderate/LeetCode15.py
+++ Moderate/LeetCode15.py
@@ -1,54 +1,56 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        n=len(nums)
-        res=[]
-        if(not nums or n<3):
-            return []
-        nums.sort()
-        res=[]
-        for i in range(n):
-            if(nums[i]>0):
-                return res
-            if(i>0 and nums[i]==nums[i-1]):
-                continue
-            L=i+1
-            R=n-1
-            while(L<R):
-                if(nums[i]+nums[L]+nums[R]==0):
-                    res.append([nums[i],nums[L],nums[R]])
-                    while(L<R and nums[L]==nums[L+1]):
-                        L=L+1
-                    while(L<R and nums[R]==nums[R-1]):
-                        R=R-1
-                    L=L+1
-                    R=R-1
-                elif(nums[i]+nums[L]+nums[R]>0):
-                    R=R-1
-                else:
-                    L=L+1
-        return res
 
-class Solution:
-    def threeSum(self, nums: [int]) -> [[int]]:
-        nums.sort()
-        res, k = [], 0
-        for k in range(len(nums) - 2):
-            if nums[k] > 0: break # 1. because of j > i > k.
-            if k > 0 and nums[k] == nums[k - 1]: continue # 2. skip the same `nums[k]`.
-            i, j = k + 1, len(nums) - 1
-            while i < j: # 3. double pointer
-                s = nums[k] + nums[i] + nums[j]
-                if s < 0:
-                    i += 1
-                    while i <= j and nums[i] == nums[i - 1]: i += 1
-                elif s > 0:
-                    j -= 1
-                    while i < j and nums[j] == nums[j + 1]: j -= 1
-                else:
-                    res.append([nums[k], nums[i], nums[j]])
-                    i += 1
-                    j -= 1
-                    while i < j and nums[i] == nums[i - 1]: i += 1
-                    while i < j and nums[j] == nums[j + 1]: j -= 1
-        return res
+
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+#         n=len(nums)
+#         res=[]
+#         if(not nums or n<3):
+#             return []
+#         nums.sort()
+#         res=[]
+#         for i in range(n):
+#             if(nums[i]>0):
+#                 return res
+#             if(i>0 and nums[i]==nums[i-1]):
+#                 continue
+#             L=i+1
+#             R=n-1
+#             while(L<R):
+#                 if(nums[i]+nums[L]+nums[R]==0):
+#                     res.append([nums[i],nums[L],nums[R]])
+#                     while(L<R and nums[L]==nums[L+1]):
+#                         L=L+1
+#                     while(L<R and nums[R]==nums[R-1]):
+#                         R=R-1
+#                     L=L+1
+#                     R=R-1
+#                 elif(nums[i]+nums[L]+nums[R]>0):
+#                     R=R-1
+#                 else:
+#                     L=L+1
+#         return res
+
+# class Solution:
+#     def threeSum(self, nums: [int]) -> [[int]]:
+#         nums.sort()
+#         res, k = [], 0
+#         for k in range(len(nums) - 2):
+#             if nums[k] > 0: break # 1. because of j > i > k.
+#             if k > 0 and nums[k] == nums[k - 1]: continue # 2. skip the same `nums[k]`.
+#             i, j = k + 1, len(nums) - 1
+#             while i < j: # 3. double pointer
+#                 s = nums[k] + nums[i] + nums[j]
+#                 if s < 0:
+#                     i += 1
+#                     while i <= j and nums[i] == nums[i - 1]: i += 1
+#                 elif s > 0:
+#                     j -= 1
+#                     while i < j and nums[j] == nums[j + 1]: j -= 1
+#                 else:
+#                     res.append([nums[k], nums[i], nums[j]])
+#                     i += 1
+#                     j -= 1
+#                     while i < j and nums[i] == nums[i - 1]: i += 1
+#                     while i < j and nums[j] == nums[j + 1]: j -= 1
+#         return res
