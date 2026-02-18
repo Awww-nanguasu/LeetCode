af9a99663f5c50c6e6df4fc339fc0fd66b9929ea
Awww-nanguasu <2328194470@qq.com>
2026-02-18 19:57:53 +0800

    '[Easy] preorder LeetCode590

---
+++ Easy/LeetCode590.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def __init__(self):
+        self.List = []
+
+    def postorder(self, root: 'Node') -> List[int]:
+        if root != None:
+            for x in root.children:
+                self.postorder(x)
+            
+            self.List.append(root.val)
+        return self.List
