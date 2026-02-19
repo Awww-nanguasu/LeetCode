d3cd7768b6bcf22f826e502eef01f275ebf8ab43
Awww-nanguasu <2328194470@qq.com>
2026-02-19 15:47:31 +0800

    '[Easy] minDepth LeetCode111'

--- Easy/LeetCode118.py
+++ Easy/LeetCode118.py
@@ -11,12 +11,6 @@ class Solution:
             ret.append(row)
         return ret
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if root == None:
