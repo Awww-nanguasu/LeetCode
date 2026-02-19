52b40f378e217f0295cddd58e5563d67d61d0dfb
Awww-nanguasu <2328194470@qq.com>
2026-02-19 15:11:49 +0800

    '[Easy] maxDepth LeetCode104'

--- Easy/LeetCode104.py
+++ Easy/LeetCode104.py
@@ -25,3 +25,18 @@ class Solution:
             queue = tmp
             res += 1
         return res
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        else:
+            length_left = self.maxDepth(root.left)
+            length_right = self.maxDepth(root.right)
+            return max(length_left, length_right)+1
