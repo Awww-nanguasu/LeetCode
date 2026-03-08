dedb523e447026cdfebbf3f52bf1a376ec31ba28
Awww-nanguasu <2328194470@qq.com>
2026-03-08 10:34:08 +0800

    '[Moderate] flatten LeetCode114'

--- Moderate/LeetCode114.py
+++ Moderate/LeetCode114.py
@@ -20,3 +20,28 @@ class Solution:
             root.right = root.left
             root.right.right = tmp
 
+class Solution:
+    # 定义：将以 root 为根的树拉平为链表
+    def flatten(self, root) -> None:
+        # base case
+        if root is None:
+            return
+
+        # 利用定义，把左右子树拉平
+        self.flatten(root.left)
+        self.flatten(root.right)
+
+        # 后序遍历位置
+        # 1、左右子树已经被拉平成一条链表
+        left = root.left
+        right = root.right
+
+        # 2、将左子树作为右子树
+        root.left = None
+        root.right = left
+
+        # 3、将原先的右子树接到当前右子树的末端
+        p = root
+        while p.right is not None:
+            p = p.right
+        p.right = right
