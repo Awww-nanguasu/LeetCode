b781cc465bbe7a39e79f8dc5176adaaa69345f10
Awww-nanguasu <2328194470@qq.com>
2026-05-25 23:15:03 +0800

    [Moderate] LeetCode44 isMatch

--- Moderate/LeetCode44.py
+++ Moderate/LeetCode44.py
@@ -20,3 +20,25 @@ class Solution:
                 return False
                 
         return all(x == "*" for x in p[j:])
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        
+        m, n = len(s), len(p)
+        dp = [[False] * (n+1) for _ in range(m+1)]
+        
+        dp[0][0] = True
+        for j in range(1, n+1): 
+            if p[j-1] == '*':
+                dp[0][j] = True
+            else:
+                break
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s[i-1] == p[j-1] or p[j-1] == '?':  
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':            
+                    dp[i][j] = dp[i][j-1] | dp[i-1][j]  
+        
+        return dp[m][n]
