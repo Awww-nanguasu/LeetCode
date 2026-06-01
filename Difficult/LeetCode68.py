9b0dfe96f60abe1fe21c3faad24fe904be0b41a2
Awww-nanguasu <2328194470@qq.com>
2026-06-01 22:50:13 +0800

    [Difficult] fullJustify LeetCode68

--- Difficult/LeetCode68.py
+++ Difficult/LeetCode68.py
@@ -35,3 +35,15 @@ class Solution(object):
             i += 1
         return res
 
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        res, line, str_num = [], [], 0
+        for word in words:
+            if str_num + len(line)-1 + len(word) >= maxWidth:
+                for i in range(maxWidth - str_num):
+                    line[i%max(len(line)-1, 1)] += ' '     
+                res.append(''.join(line))
+                line, str_num = [], 0         
+            line.append(word)
+            str_num += len(word)
+        return res + [' '.join(line).ljust(maxWidth)]
