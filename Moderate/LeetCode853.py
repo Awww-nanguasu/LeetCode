b7f719d8055c0caaf61603f74e329e1e7c906e98
Awww-nanguasu <2328194470@qq.com>
2026-08-21 17:24:51 +0800

    pvlof code

--- Moderate/LeetCode853.py
+++ Moderate/LeetCode853.py
@@ -14,3 +14,20 @@ class Solution:
             st.append(i)
 
         return len(st)
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(position)
+        position = sorted(enumerate(position), key=lambda x:x[1])
+        times = []
+
+        for id, pos in position:
+            times.append((target - pos)/speed[id])
+
+        st = []
+        for i in range(n):
+            while st and times[st[-1]] <= times[i]:
+                st.pop()
+            st.append(i)
+
+        return len(st)
